b90b0c9d9e2d0806d9d8d9393b8c48f04f40693a
lexerAnn <52282400+lexerAnn@users.noreply.github.com>
2021-06-06 16:43:42 +0100

    Update master.php

--- master.php
+++ master.php
@@ -497,6 +497,7 @@ $("#progressText").html("Step-3");
 <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/popper.min.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
+		
 	
   <!-- simplebar js -->
   <script src="assets/plugins/simplebar/js/simplebar.js"></script>
@@ -506,6 +507,7 @@ $("#progressText").html("Step-3");
   <script src="assets/js/sidebar-menu.js"></script>
   <!-- Custom scripts -->
   <script src="assets/js/app-script.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>		
   
   <!-- Vector map JavaScript -->
   <script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
