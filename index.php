1ad2b32a6c65920c5ef826b9090019ffc08de91b
lexerAnn <52282400+lexerAnn@users.noreply.github.com>
2021-07-08 00:18:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -28,6 +28,22 @@
 	<style type="text/css">
 
 	</style>
+	<!-- Bootstrap core JavaScript-->
+<script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+	
+  <!-- simplebar js -->
+  <script src="assets/plugins/simplebar/js/simplebar.js"></script>
+
+  <!-- sidebar-menu js -->
+  <script src="assets/js/sidebar-menu.js"></script>
+  <!-- Custom scripts -->
+  <script src="assets/js/app-script.js"></script>
+  
+  
+  <!-- Chart js -->
+  <script src="assets/plugins/Chart.js/Chart.min.js"></script>
 </head>
 <body class="bg-white">
 	<!-- Start wrapper-->
@@ -161,22 +177,7 @@ else
 	});
 </script>
 
-<!-- Bootstrap core JavaScript-->
-<script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/popper.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-	
-  <!-- simplebar js -->
-  <script src="assets/plugins/simplebar/js/simplebar.js"></script>
 
-  <!-- sidebar-menu js -->
-  <script src="assets/js/sidebar-menu.js"></script>
-  <!-- Custom scripts -->
-  <script src="assets/js/app-script.js"></script>
-  
-  
-  <!-- Chart js -->
-  <script src="assets/plugins/Chart.js/Chart.min.js"></script>
 
 </body>
 </html>
