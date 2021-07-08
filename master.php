ada75f5005065c000e9ee2e5458e815a1dc7030d
lexerAnn <52282400+lexerAnn@users.noreply.github.com>
2021-07-08 01:32:18 +0100

    Update master.php

--- master.php
+++ master.php
@@ -30,6 +30,23 @@
 #second,#third,#fourth{display: none}
 
 	</style>
+	<!-- Bootstrap core JavaScript-->
+<script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+	
+  <!-- simplebar js -->
+  <script src="assets/plugins/simplebar/js/simplebar.js"></script>
+  <!-- waves effect js -->
+  <script src="assets/js/waves.js"></script>
+  <!-- sidebar-menu js -->
+  <script src="assets/js/sidebar-menu.js"></script>
+  <!-- Custom scripts -->
+  <script src="assets/js/app-script.js"></script>
+  
+
+  <!-- Chart js -->
+  <script src="assets/plugins/Chart.js/Chart.min.js"></script>
 </head>
 <body class="bg-white">
 	<!-- Start wrapper-->
@@ -549,25 +566,7 @@ $("#progressText").html("Step-3");
 	});
 </script>
 
-<!-- Bootstrap core JavaScript-->
-<script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/popper.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-	
-  <!-- simplebar js -->
-  <script src="assets/plugins/simplebar/js/simplebar.js"></script>
-  <!-- waves effect js -->
-  <script src="assets/js/waves.js"></script>
-  <!-- sidebar-menu js -->
-  <script src="assets/js/sidebar-menu.js"></script>
-  <!-- Custom scripts -->
-  <script src="assets/js/app-script.js"></script>
-  
-  <!-- Vector map JavaScript -->
-  <script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-  <script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
-  <!-- Chart js -->
-  <script src="assets/plugins/Chart.js/Chart.min.js"></script>
+
 
 </body>
 </html>
